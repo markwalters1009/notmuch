5c24c95a1fb5f2ff12caf3daaf6ae0d967a07605
Mark Walters <markwalters1009@gmail.com>
2012-05-28 01:01:05 +0100

    emacs: add stub pick.el file
    We add a stub pick.el file to minimise the impact on the main code
    whilst making it easy for people to replace it with the full pick file
    from contrib.

---
+++ emacs/notmuch-pick.el
@@ -0,0 +1,37 @@
+;; notmuch-pick.el --- displaying notmuch forests.
+;;
+;; This is just a stub file until the main file is clean enough to be
+;; merged.
+;;
+;; Copyright © Carl Worth
+;; Copyright © David Edmondson
+;; Copyright © Mark Walters
+;;
+;; This file is part of Notmuch.
+;;
+;; Notmuch is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; Notmuch is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Notmuch.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;; Authors: David Edmondson <dme@dme.org>
+;;          Mark Walters <markwalters1009@gmail.com>
+
+(defun notmuch-pick-show-match-message-with-wait ()
+  (interactive)
+  t)
+
+(defun notmuch-pick (&optional query query-context buffer-name)
+  "stub function that does nothing"
+  (interactive)
+  t)
+
+(provide 'notmuch-pick)
